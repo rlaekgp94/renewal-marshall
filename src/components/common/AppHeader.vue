541b33aceaec0baf9d47330a4976fcd8b97dc928
dahye <dany__k@naver.com>
2021-06-29 04:14:05 +0900

    header fullscreen component scroll of/off

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -89,9 +89,11 @@ export default {
   methods: {
     ActiveSidebarBtn: function() {
       this.ActiveSidebar = !this.ActiveSidebar;
+      document.querySelector("body").style.overflow = "hidden";
     },
     ActiveSidebarClose: function() {
       this.ActiveSidebar = false;
+      document.querySelector("body").style.overflow = "visible";
     },
   },
 };
